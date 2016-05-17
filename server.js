d084d4f38e5e98afc960cfdf4f57be3119482dfd
jtabach <jeff.n.tabachnick@gmail.com>
2016-05-17 13:58:52 -0700

    remove client script

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('*', function(req, res) {
             <body>
                 <div id="app">${renderedBody}</div>
 
-                <script src="/static/client.js"></script>
+                
             </body>
         </html>
     `;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-    entry: './routes',
+    entry: './client',
 
     output: {
         path: path.join(__dirname, 'public/'),
