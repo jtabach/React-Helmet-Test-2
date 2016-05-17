7a928abb47082e700ac0c5017d34e7bb62e16dc0
jtabach <jeff.n.tabachnick@gmail.com>
2016-05-17 16:11:10 -0700

    test

--- client.js
+++ client.js
@@ -2,13 +2,11 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 
 let routes = require('./routes');
 
 ReactDOM.render(
-  <Router>
-    <routes />
-  </Router>,
+  <Router history={browserHistory} routes={routes} /> ,
   document.getElementById('app')
 );
