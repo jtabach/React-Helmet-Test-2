b8ec5c83923ad661ef2b9ebb1e7806e96ee089ec
jtabach <jeff.n.tabachnick@gmail.com>
2016-05-17 21:14:15 -0700

    favicon w e

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.set('view engine', 'ejs');
 
 /* a single request handler receives every server request
    and routes through react-router */
+app.get('/favicon.ico', (req, res) => res.send(''));
 app.get('/*', function(req, res) {
     /* create a router and give it our routes
        and the requested path */
